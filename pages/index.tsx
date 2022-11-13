931a3c6df99699ae49edc0f60fce39026a433cd9
R.K <96303806+Ycamera@users.noreply.github.com>
2022-11-13 15:04:01 +0900

    added some meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, Button, Heading, Grid } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
-
+import Head from "next/head";
 import { ImageProps, InfoProps } from "../components/type/type";
 import UploadImages from "../components/UploadImage";
 import CommonForm from "../components/CommonForm";
@@ -90,49 +90,61 @@ export default function Home() {
 	};
 
 	return (
-		<useFormContext.Provider value={{ register, errors }}>
-			<LoadingBg turnBgIntoDark={turnBgIntoDark} />
-			<Box w="calc(100% - 2rem)" maxW="50rem" mx="auto" my={{ base: "2rem", sm: "4rem" }}>
-				<Heading textAlign="center" color="gray.600" as="h1" fontSize="2rem">
-					資料作成ソフト
-				</Heading>
-				<Box
-					boxShadow="0 0 0.2rem gray"
-					p={{ base: "1rem", sm: "3rem" }}
-					borderRadius="1rem"
-					mt={{ base: "2rem", sm: "4rem" }}
-				>
-					<form>
-						<Box w="100%" mx="auto" mt="1rem">
-							<CommonForm label="現場名:" onChange={placeChangeHandler} value={info.place} name="place" />
+		<>
+			<Head>
+				<title>資料作成ソフト</title>
+				<meta name="author" content="Ryosuke.K" />
+				<meta name="description" content="草刈り業務の報告用資料作成ソフト" />
+			</Head>
+			<useFormContext.Provider value={{ register, errors }}>
+				<LoadingBg turnBgIntoDark={turnBgIntoDark} />
+				<Box w="calc(100% - 2rem)" maxW="50rem" mx="auto" my={{ base: "2rem", sm: "4rem" }}>
+					<Heading textAlign="center" color="gray.600" as="h1" fontSize="2rem">
+						資料作成ソフト
+					</Heading>
+					<Box
+						boxShadow="0 0 0.2rem gray"
+						p={{ base: "1rem", sm: "3rem" }}
+						borderRadius="1rem"
+						mt={{ base: "2rem", sm: "4rem" }}
+					>
+						<form>
+							<Box w="100%" mx="auto" mt="1rem">
+								<CommonForm
+									label="現場名:"
+									onChange={placeChangeHandler}
+									value={info.place}
+									name="place"
+								/>
 
-							<Grid {...gridStyle}>
-								<CommonForm
-									label="図面No."
-									onChange={(e: any) => numberChangeHandler(e, "number")}
-									value={info.number}
-									name="number"
-								/>
-								<CommonForm
-									label="回数"
-									onChange={(e: any) => numberChangeHandler(e, "time")}
-									value={info.time}
-									name="time"
-								/>
+								<Grid {...gridStyle}>
+									<CommonForm
+										label="図面No."
+										onChange={(e: any) => numberChangeHandler(e, "number")}
+										value={info.number}
+										name="number"
+									/>
+									<CommonForm
+										label="回数"
+										onChange={(e: any) => numberChangeHandler(e, "time")}
+										value={info.time}
+										name="time"
+									/>
+								</Grid>
+							</Box>
+							<UploadImages images={images} imageChangeHandler={imageChangeHandler} />
+							<Grid mt="3rem" {...gridStyle} templateColumns="repeat(2,1fr)" gap="1rem">
+								<Button colorScheme="orange" onClick={resetForm}>
+									リセット
+								</Button>
+								<Button colorScheme="messenger" onClick={handleSubmit(onSubmit)}>
+									ダウンロード
+								</Button>
 							</Grid>
-						</Box>
-						<UploadImages images={images} imageChangeHandler={imageChangeHandler} />
-						<Grid mt="3rem" {...gridStyle} templateColumns="repeat(2,1fr)" gap="1rem">
-							<Button colorScheme="orange" onClick={resetForm}>
-								リセット
-							</Button>
-							<Button colorScheme="messenger" onClick={handleSubmit(onSubmit)}>
-								ダウンロード
-							</Button>
-						</Grid>
-					</form>
+						</form>
+					</Box>
 				</Box>
-			</Box>
-		</useFormContext.Provider>
+			</useFormContext.Provider>
+		</>
 	);
 }
